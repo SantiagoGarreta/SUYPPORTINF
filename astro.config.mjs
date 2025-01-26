49359dad4b209ecc864d4f00ba2890988d2935b2
Santiago Garreta <santiagogarreta6@gmail.com>
2025-01-26 16:50:25 -0300

    workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://SantiagoGarreta.github.io',
+	base: "SUYPPORTINF",
 	integrations: [mdx(), sitemap()],
 });
